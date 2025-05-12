1dcce1381c15b3905740d9f22bf0a6ddd72b7f65
Otavio <otaviomiranda@usp.br>
2025-05-11 22:37:38 -0300

    implementa get

--- main.c
+++ main.c
@@ -4,10 +4,28 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <regex.h>
+#include <strings.h>
+#include <asm-generic/socket.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #define PORT 8080
 #define BUFFER_LEN 1024
 
+
+char *get_mime_type(const char *file_ext) {
+    if (strcasecmp(file_ext, "html") == 0 || strcasecmp(file_ext, "htm") == 0) {
+        return "text/html"; 
+    } else if (strcasecmp(file_ext, "txt") == 0) {
+        return "text/plain";
+    } else if (strcasecmp(file_ext, "jpg") == 0) {
+        return "image/jpeg";
+    } else if (strcasecmp(file_ext, "png") == 0) {
+        return "image/png";
+    } else {
+        return "application/octet-stream";
+    }
+}
 char *url_decode(const char *src) {
     size_t src_len = strlen(src);
     char *decoded = malloc(src_len + 1);
@@ -25,6 +43,56 @@ char *url_decode(const char *src) {
     }
     return decoded;    
 }
+
+void build_http_response(const char *file_name,
+     const char *file_ext,
+     char *response,
+     size_t *response_len) {
+    //comparar se a extensão é igual a html/htm/txt/jpg/png
+    char *mime_type = get_mime_type(file_ext);
+    char *header = (char *) malloc(BUFFER_LEN * sizeof(char));
+    snprintf(header, BUFFER_LEN,
+        "HTTP/1.1 200 OK\r\n"
+        "Content-type: %s\r\n"
+        "\r\n",
+        mime_type);
+    //se o recurso não existir, resposta é 404 Not Found
+    if (strcmp(file_name, ".well-known/appspecific/com.chrome.devtools.json") == 0) {
+        snprintf(response,BUFFER_LEN,
+            "HTTP/1.1 404 Not Found\r\n"
+            "Content-type: text/plain\r\n"
+            "Connection: close\r\n"
+            "\r\n"
+            "404 Not Found\n"
+        );
+    }
+    int file_fd = open(file_name, O_RDONLY);
+    if (file_fd == -1) {
+        snprintf(response, BUFFER_LEN,
+            "HTTP/1.1 404 Not Found\r\n"
+            "Content-type: text/plain\r\n"
+            "\r\n"
+            "404 Not Found\n");
+            // *response_len = strlen(response);
+            return;
+        }
+        //pega o file size para Content-Length
+        struct stat file_stat;
+        fstat(file_fd, &file_stat);
+        off_t file_size = file_stat.st_size;
+
+        //copy header to response buffer
+        *response_len = 0;
+        memcpy(response, header, strlen(header));
+        *response_len += strlen(header);
+        size_t bytes_read;
+        while ((bytes_read = read(file_fd, response + *response_len, BUFFER_LEN - *response_len)) > 0) {
+            *response_len += bytes_read;
+        }
+        free(header);
+        close(file_fd);
+        
+    }
 
 char *get_file_extension(const char *file_name) {
     char *dot = strrchr(file_name, '.');
@@ -67,6 +135,8 @@ int main() {
     listen(server_fd, 3); //Faz uma fila de 3 antes de retornar que esta com a fila cheia
 
     printf("Web serve listening on port 8080\n");
+    
+
     while (1) {
         //aceita a conexao e cria um novo socket para conversar com o client
         client_fd = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
@@ -74,7 +144,7 @@ int main() {
         if (byteCount > 0) {
             // printf("Received message:\n %s\n", buffer);
         }
-        write(client_fd, html_response, strlen(html_response));
+        // write(client_fd, html_response, strlen(html_response));
         regex_t regex;
         regcomp(&regex, "GET /([^ ]*) HTTP/1", REG_EXTENDED);
         regmatch_t matches[2];
@@ -86,15 +156,16 @@ int main() {
             char *file_name = url_decode(url_encoded_file_name);
             char file_ext[32];
             strcpy(file_ext, get_file_extension(file_name));
-            printf("file_ext: %s\n", file_ext);
-
-
-
-
+            char *response = (char *)malloc(BUFFER_LEN * 2 * sizeof(char));
+            size_t response_len;
+            build_http_response(file_name, file_ext, response, &response_len);
+            //send http response to client
+            send(client_fd, response, response_len, 0);
+            free(response);
+            free(file_name);
         }
-        close(client_fd);
-    }
-    
+        regfree(&regex);
+    }    
     close(server_fd);
     return 0;
 }
